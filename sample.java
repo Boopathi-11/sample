8474e1fb817bf172778dfeb2e00c925b3a496aa7
Boopathi-11 <130132043+Boopathi-11@users.noreply.github.com>
2023-04-13 10:22:34 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,4 +1,4 @@
-public class hello {
+public class sample {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
             System.out.println("Hello Worlds " + i);
